649144e8d72b0c559344ec6727f19453b98c2428
mohammed soud <smdsoud@gmail.com>
2026-03-06 15:14:53 +0530

    fix render host issue

--- vite.config.ts
+++ vite.config.ts
@@ -3,30 +3,31 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      server: {
-        port: 3000,
-        host: '0.0.0.0',
-      },
+  const env = loadEnv(mode, '.', '');
 
-      plugins: [react()],
+  return {
+    server: {
+      port: 3000,
+      host: '0.0.0.0',
+      allowedHosts: ['aqua-intel-prediction-3.onrender.com']
+    },
 
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
-      },
+    preview: {
+      host: '0.0.0.0',
+      allowedHosts: ['aqua-intel-prediction-3.onrender.com']
+    },
 
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
-      },
+    plugins: [react()],
 
-      // 👇 ADD THIS PART
-      preview: {
-        allowedHosts: true
+    define: {
+      'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
+    },
+
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, '.'),
       }
-
-    };
-});
+    }
+  }
+})
