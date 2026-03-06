7f7d0be862153aeb3f04e9505f0c118908cdfcc5
mohammed soud <smdsoud@gmail.com>
2026-03-06 15:08:57 +0530

    fix render host

--- vite.config.ts
+++ vite.config.ts
@@ -9,15 +9,24 @@ export default defineConfig(({ mode }) => {
         port: 3000,
         host: '0.0.0.0',
       },
+
       plugins: [react()],
+
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
       },
+
       resolve: {
         alias: {
           '@': path.resolve(__dirname, '.'),
         }
+      },
+
+      // 👇 ADD THIS PART
+      preview: {
+        allowedHosts: true
       }
+
     };
 });
